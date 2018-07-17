75f0dd1e33c1a17b27d59484b59d6c3c7de2ff2c
Julia <dart1ju@gmail.com>
2018-07-17 17:10:39 -0400

    Added comments

--- src/Lab3.java
+++ src/Lab3.java
@@ -6,15 +6,21 @@ public class Lab3 {
 		// This program will ask user for number between 1 and 100 and return various
 		// results
 		Scanner scan = new Scanner(System.in);
+		// Asks user for name info
 		System.out.println("What is your name?");
 		String name = scan.nextLine();
 		String decision = "";
+		// Do loop will allow user to continue entering numbers until indicating
+		// otherwise
 		do {
 		
 		System.out.println("Hi " + name + ", please enter a number between 1 and 100.");
 		int userNum = scan.nextInt();
+			// validates if number entered is in desired range
 		if (userNum >= 1 && userNum <= 100) {
-		if (userNum % 2 == 0) {
+				// checks for even number and prints statements based on where it falls in
+				// ranges
+				if (userNum % 2 == 0) {
 			if (userNum > 60) {
 					System.out.println("Number is " + userNum + " and is even.");
 			} else if (userNum >= 26) {
@@ -23,6 +29,7 @@ public class Lab3 {
 					System.out.println("Even and less than 25.");
 			}
 		}
+				// checks for odd number and prints statements based on where it falls in ranges
 		if (userNum % 2 != 0) {
 			if (userNum > 60) {
 					System.out.println("Number is " + userNum + " and is odd and over 60.");
@@ -30,18 +37,19 @@ public class Lab3 {
 					System.out.println("Number is " + userNum + " and is odd.");
 			}
 		}
-			System.out.println("Continue?");
+				// Prompts user to continue or not
+				System.out.println("Continue? (y/n)");
 
 			decision = scan.next();
 
 		}
-
+			// executes if number is not in valid range
 		else {
 			System.out.println(name + ", please enter a valid number.");
 		}
 		} while (decision.equalsIgnoreCase("y"));
 
-
+		System.out.println("Goodbye"); // end program
 		scan.close();
 	}
 
